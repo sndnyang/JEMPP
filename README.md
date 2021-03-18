66701ea0e804972e168da717b1f7154808a8143f
sndnyang <xiulong.yancey.yang@gmail.com>
2021-03-18 14:59:08 -0400

    readme and basic utils

---
+++ README.md
@@ -0,0 +1,60 @@
+# JEM++/PYLD - Improved Joint Energy Models and SGLD
+
+
+
+Includes scripts for training JEM (Joint-Energy Model), evaluating models at various tasks, and running adversarial attacks.
+
+## Usage
+### Training
+To train a model on CIFAR10 as in the paper
+```markdown
+python train_wrn_jempp.py --lr .0001 --dataset cifar10 --optimizer adam --p_x_weight 1.0 --p_y_given_x_weight 1.0 --p_x_y_weight 0.0 --sigma .03 --width 10 --depth 28 --plot_uncond --warmup_iters 1000
+```
+
+### Evaluation
+
+To evaluate the classifier (on CIFAR10):
+```markdown
+python eval_wrn_jempp.py --load_path /PATH/TO/YOUR/MODEL.pt --eval test_clf --dataset cifar_test
+```
+To do OOD detection (on CIFAR100)
+```markdown
+python eval_wrn_jempp.py --load_path /PATH/TO/YOUR/MODEL.pt --eval OOD --ood_dataset cifar_100
+```
+To generate a histogram of OOD scores like Table 2
+```markdown
+python eval_wrn_jempp.py --load_path /PATH/TO/YOUR/MODEL.pt --eval logp_hist --datasets cifar10 svhn 
+```
+
+To generate the xxx in the gif
+```shell script
+python eval_wrn_jempp.py --eval uncond_samples --n_sample_steps 100 --print_every 1 --reinit_freq 0 --model yopo --norm batch --load_path /path/to/your.pt  --n_steps 40 --buffer_size 100 --batch_size 100 --sgld_std 0
+```
+
+To generate new unconditional samples
+```markdown
+python eval_wrn_jempp.py --load_path /PATH/TO/YOUR/MODEL.pt --eval uncond_samples --n_sample_steps {THE_MORE_THE_BETTER (1000 minimum)} --buffer_size 10000 --n_steps 40 --print_every 100 --reinit_freq 0.05
+```
+To generate conditional samples from a saved replay buffer
+```markdown
+python eval_wrn_jempp.py --load_path /PATH/TO/YOUR/MODEL.pt --eval cond_samples 
+```
+To generate new conditional samples
+```markdown
+python eval_wrn_jempp.py --load_path /PATH/TO/YOUR/MODEL.pt --eval cond_samples --n_sample_steps {THE_MORE_THE_BETTER (1000 minimum)} --buffer_size 10000 --n_steps 40 --print_every 10 --reinit_freq 0.05 --fresh_samples
+ ```
+
+
+### Attacks
+
+To run Linf attacks on JEM-1
+```markdown
+python attack_model.py --start_batch 0 --end_batch 6 --load_path /PATH/TO/YOUR/MODEL.pt --exp_name /YOUR/EXP/NAME --n_steps_refine 1 --distance Linf --random_init --n_dup_chains 5 --base_dir /PATH/TO/YOUR/EXPERIMENTS/DIRECTORY
+```
+To run L2 attacks on JEM-1
+```markdown
+python attack_model.py --start_batch 0 --end_batch 6 --load_path /cloud_storage/BEST_jempp.pt --exp_name rerun_jempp_1_step_5_dup_l2_no_sigma_REDO --n_steps_refine 1 --distance L2 --random_init --n_dup_chains 5 --sigma 0.0 --base_dir /cloud_storage/adv_results &
+ ```
+ 
+
+Happy Energy-Based Modeling! 
---
+++ utils.py
@@ -0,0 +1,221 @@
+import os
+import torch
+import torch as t
+import torch.nn as nn
+import torchvision as tv
+import torchvision.transforms as tr
+from torch.utils.data import DataLoader, Dataset
+import numpy as np
+from torch.nn.modules.loss import _Loss
+from ExpUtils import AverageMeter
+
+
+class Hamiltonian(_Loss):
+
+    def __init__(self, layer, reg_cof=1e-4):
+        super(Hamiltonian, self).__init__()
+        self.layer = layer
+        self.reg_cof = 0
+
+    def forward(self, x, p):
+
+        y = self.layer(x)
+        H = torch.sum(y * p)
+        # H = H - self.reg_cof * l2
+        return H
+
+
+def sqrt(x):
+    return int(t.sqrt(t.Tensor([x])))
+
+
+def plot(p, x):
+    return tv.utils.save_image(t.clamp(x, -1, 1), p, normalize=True, nrow=sqrt(x.size(0)))
+
+
+def makedirs(dirname):
+    if not os.path.exists(dirname):
+        os.makedirs(dirname)
+
+
+def save_checkpoint(state, save, epoch):
+    if not os.path.exists(save):
+        os.makedirs(save)
+    filename = os.path.join(save, 'checkpt-%04d.pth' % epoch)
+    torch.save(state, filename)
+
+
+class DataSubset(Dataset):
+    def __init__(self, base_dataset, inds=None, size=-1):
+        self.base_dataset = base_dataset
+        if inds is None:
+            inds = np.random.choice(list(range(len(base_dataset))), size, replace=False)
+        self.inds = inds
+
+    def __getitem__(self, index):
+        base_ind = self.inds[index]
+        return self.base_dataset[base_ind]
+
+    def __len__(self):
+        return len(self.inds)
+
+
+def cycle(loader):
+    while True:
+        for data in loader:
+            yield data
+
+
+def init_random(args, bs, im_sz=32, n_ch=3):
+    return t.FloatTensor(bs, n_ch, im_sz, im_sz).uniform_(-1, 1)
+
+
+def get_data(args):
+    if args.dataset == "svhn":
+        transform_train = tr.Compose(
+            [tr.Pad(4, padding_mode="reflect"),
+             tr.RandomCrop(32),
+             tr.ToTensor(),
+             tr.Normalize((.5, .5, .5), (.5, .5, .5)),
+             lambda x: x + args.sigma * t.randn_like(x)]
+        )
+    else:
+        transform_train = tr.Compose(
+            [tr.Pad(4, padding_mode="reflect"),
+             tr.RandomCrop(32),
+             tr.RandomHorizontalFlip(),
+             tr.ToTensor(),
+             tr.Normalize((.5, .5, .5), (.5, .5, .5)),
+             lambda x: x + args.sigma * t.randn_like(x)]
+        )
+    transform_test = tr.Compose(
+        [tr.ToTensor(),
+         tr.Normalize((.5, .5, .5), (.5, .5, .5))]
+    )
+
+    def dataset_fn(train, transform):
+        if args.dataset == "cifar10":
+            return tv.datasets.CIFAR10(root=args.data_root, transform=transform, download=True, train=train)
+        elif args.dataset == "cifar100":
+            return tv.datasets.CIFAR100(root=args.data_root, transform=transform, download=True, train=train)
+        else:
+            return tv.datasets.SVHN(root=args.data_root, transform=transform, download=True, split="train" if train else "test")
+
+    # get all training inds
+    full_train = dataset_fn(True, transform_train)
+    all_inds = list(range(len(full_train)))
+    # set seed
+    np.random.seed(1234)
+    # shuffle
+    np.random.shuffle(all_inds)
+    # seperate out validation set
+    if args.n_valid > args.n_classes:
+        valid_inds, train_inds = all_inds[:args.n_valid], all_inds[args.n_valid:]
+    else:
+        valid_inds, train_inds = [], all_inds
+    train_inds = np.array(train_inds)
+    train_labeled_inds = []
+    other_inds = []
+    if args.labels_per_class > 0:
+        train_labels = np.array([full_train[ind][1] for ind in train_inds])  # to speed up
+        for i in range(args.n_classes):
+            print(i)
+            train_labeled_inds.extend(train_inds[train_labels == i][:args.labels_per_class])
+            other_inds.extend(train_inds[train_labels == i][args.labels_per_class:])
+    else:
+        train_labeled_inds = train_inds
+
+    dset_train = DataSubset(dataset_fn(True, transform_train), inds=train_inds)
+    dset_train_labeled = DataSubset(dataset_fn(True, transform_train), inds=train_labeled_inds)
+    dset_valid = DataSubset(dataset_fn(True, transform_test), inds=valid_inds)
+
+    num_workers = 0 if args.debug else 4
+    dload_train = DataLoader(dset_train, batch_size=args.batch_size, shuffle=True, num_workers=num_workers, drop_last=True)
+    dload_train_labeled = DataLoader(dset_train_labeled, batch_size=args.batch_size, shuffle=True, num_workers=num_workers, drop_last=True)
+    dload_train_labeled = cycle(dload_train_labeled)
+    dset_test = dataset_fn(False, transform_test)
+    dload_valid = DataLoader(dset_valid, batch_size=100, shuffle=False, num_workers=num_workers, drop_last=False)
+    dload_test = DataLoader(dset_test, batch_size=100, shuffle=False, num_workers=num_workers, drop_last=False)
+    return dload_train, dload_train_labeled, dload_valid, dload_test
+
+
+def accuracy(output, target, topk=(1,)):
+    """Computes the accuracy over the k top predictions for the specified values of k"""
+    with torch.no_grad():
+        maxk = max(topk)
+        batch_size = target.size(0)
+
+        _, pred = output.topk(maxk, 1, True, True)
+        pred = pred.t()
+        correct = pred.eq(target.view(1, -1).expand_as(pred))
+
+        res = []
+        for k in topk:
+            correct_k = correct[:k].view(-1).float().sum(0, keepdim=True)
+            res.append(correct_k.mul_(100.0 / batch_size))
+        return res
+
+
+def checkpoint(f, buffer, component, tag, args, device):
+    f.cpu()
+    ckpt_dict = {
+        "model_state_dict": f.state_dict(),
+        "replay_buffer": buffer,
+    }
+    if component is not None:
+        ckpt_dict["margin"] = component.state_dict()
+    t.save(ckpt_dict, os.path.join(args.save_dir, tag))
+    f.to(device)
+
+
+def set_bn_eval(m):
+    if isinstance(m, nn.modules.batchnorm._BatchNorm):
+        m.eval()
+
+
+def set_bn_train(m):
+    if isinstance(m, nn.modules.batchnorm._BatchNorm):
+        m.train()
+
+
+def eval_classification(f, dload, set_name, epoch, args=None, wlog=None):
+
+    corrects, losses = [], []
+    if args.n_classes >= 200:
+        top1 = AverageMeter('Acc@1', ':6.2f')
+        top5 = AverageMeter('Acc@5', ':6.2f')
+
+    for x, y in dload:
+        x, y = x.to(args.device), y.to(args.device)
+        logits = f.classify(x)
+        loss = nn.CrossEntropyLoss(reduction='none')(logits, y).detach().cpu().numpy()
+        losses.extend(loss)
+        if args.n_classes >= 200:
+            acc1, acc5 = accuracy(logits, y, topk=(1, 5))
+            top1.update(acc1[0], x.size(0))
+            top5.update(acc5[0], x.size(0))
+        else:
+            correct = (logits.max(1)[1] == y).float().cpu().numpy()
+            corrects.extend(correct)
+        correct = (logits.max(1)[1] == y).float().cpu().numpy()
+        corrects.extend(correct)
+    loss = np.mean(losses)
+    if wlog:
+        my_print = wlog
+    else:
+        my_print = print
+    if args.n_classes >= 200:
+        correct = top1.avg
+        my_print("Epoch %d, %s loss %.5f, top1 acc %.4f, top5 acc %.4f" % (epoch, set_name, loss, top1.avg, top5.avg))
+    else:
+        correct = np.mean(corrects)
+        my_print("Epoch %d, %s loss %.5f, acc %.4f" % (epoch, set_name, loss, correct))
+    if args.vis:
+
+        args.writer.add_scalar('%s/Loss' % set_name, loss, epoch)
+        if args.n_classes >= 200:
+            args.writer.add_scalar('%s/Acc_1' % set_name, top1.avg, epoch)
+            args.writer.add_scalar('%s/Acc_5' % set_name, top5.avg, epoch)
+        else:
+            args.writer.add_scalar('%s/Accuracy' % set_name, correct, epoch)
+    return correct, loss
